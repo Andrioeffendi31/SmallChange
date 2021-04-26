00d7fab66fab710e13c864c3eb5f278fc14a6752
Andrioeffendi31 <andrioeffendi31@gmail.com>
2021-04-27 01:36:35 +0700

    Fixing UI & Backend

--- app/src/main/java/id/ac/umn/leleair/kelompok/smallchange/History.java
+++ app/src/main/java/id/ac/umn/leleair/kelompok/smallchange/History.java
@@ -3,14 +3,28 @@ package id.ac.umn.leleair.kelompok.smallchange;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 public class History extends Fragment {
     private ImageView backgroundBox;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
+    private DatabaseReference mOutcomeDatabase;
+
+    //Recycler View
+    private RecyclerView mRecyclerIncome;
+    private RecyclerView mRecyclerOutcome;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +36,23 @@ public class History extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_history, container, false);
+
         backgroundBox = view.findViewById(R.id.backgroundBoxHistory);
+        mRecyclerIncome = view.findViewById(R.id.recyclerIncome);
+        mRecyclerOutcome = view.findViewById(R.id.recyclerOutcome);
+
+        //Setup RecyclerView
+        LinearLayoutManager layoutManagerIncome = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        layoutManagerIncome.setStackFromEnd(true);
+        layoutManagerIncome.setReverseLayout(true);
+        mRecyclerIncome.setHasFixedSize(true);
+        mRecyclerIncome.setLayoutManager(layoutManagerIncome);
+
+        LinearLayoutManager layoutManagerOutcome = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        layoutManagerOutcome.setStackFromEnd(true);
+        layoutManagerOutcome.setReverseLayout(true);
+        mRecyclerOutcome.setHasFixedSize(true);
+        mRecyclerOutcome.setLayoutManager(layoutManagerOutcome);
 
         return  view;
     }
--- app/src/main/java/id/ac/umn/leleair/kelompok/smallchange/UserPanel.java
+++ app/src/main/java/id/ac/umn/leleair/kelompok/smallchange/UserPanel.java
@@ -1,18 +1,51 @@
 package id.ac.umn.leleair.kelompok.smallchange;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+import id.ac.umn.leleair.kelompok.smallchange.Model.Data;
+import id.ac.umn.leleair.kelompok.smallchange.Model.User;
 
 public class UserPanel extends Fragment {
+    private TextView username, email;
+    private Button btnChangePass, btnLogout;
     private ImageView piggy, backWallet, backgroundBox;
     private ConstraintLayout frontWallet, photoContainer;
+    private String stUsername;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference mUsername;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +56,108 @@ public class UserPanel extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_user_panel, container, false);
+
+        //Firebase Initialization
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        assert mUser != null;
+        String uid = mUser.getUid();
+
+        mUsername = FirebaseDatabase.getInstance().getReference().child("Username").child(uid);
+
         piggy = view.findViewById(R.id.piggy);
         backWallet = view.findViewById(R.id.backWallet);
         frontWallet = view.findViewById(R.id.frontWallet);
         backgroundBox = view.findViewById(R.id.backgroundBoxUP);
         photoContainer = view.findViewById(R.id.photoContainer);
+        username = view.findViewById(R.id.tvUsernameUP);
+        email = view.findViewById(R.id.tvEmailUP);
+        btnChangePass = view.findViewById(R.id.btnChangePass);
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        checkDatabaseUpdate();
+
+        btnChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changePass();
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mAuth.signOut();
+                    moveToLogin();
+                } catch (Exception e) {
+                    Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), "Logout Failed, please check your internet connection", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         return view;
+    }
+
+    public void changePass() {
+        //Change Password Form
+        Dialog mdialog = new Dialog(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View myviewm = inflater.inflate(R.layout.change_pass_form, null);
+        mdialog.setContentView(myviewm);
+        mdialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        int width = (int)(getResources().getDisplayMetrics().widthPixels*0.90);
+        int height = (int)(getResources().getDisplayMetrics().heightPixels*0.38);
+
+        mdialog.getWindow().setLayout(width, height);
+
+        EditText editNewPass = myviewm.findViewById(R.id.tvNewPass);
+
+        Button btnChange = myviewm.findViewById(R.id.btnChangePass);
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newPass = editNewPass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(newPass)){
+                    editNewPass.setError("Required Field");
+                    return;
+                }
+            }
+        });
+        mdialog.show();
+    }
+
+    private void checkDatabaseUpdate() {
+        String authEmail;
+
+        //get auth Email
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null){
+            FirebaseUser mUser = mAuth.getCurrentUser();
+            authEmail = mUser.getEmail();
+            email.setText(authEmail);
+        }
+
+        mUsername.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                stUsername = "";
+
+                for(DataSnapshot mSnapshot:snapshot.getChildren()){
+                    User user = mSnapshot.getValue(User.class);
+                    assert  user != null;
+                    stUsername = user.getUsername();
+                }
+                username.setText(stUsername);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public void playAnimOut(){
@@ -45,5 +174,11 @@ public class UserPanel extends Fragment {
         piggy.animate().translationY(0).alpha(1).setDuration(600).setStartDelay(400);
         backWallet.animate().translationY(0).alpha(1).setDuration(600);
         frontWallet.animate().translationY(0).alpha(1).setDuration(600);
+    }
+
+    private void moveToLogin() {
+        Intent intent = new Intent(getActivity(), Login.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
